f2db89897049e47642cc283e8cab35411e2b98dc
Gabriel Assana <gabrielhhz2@gmail.com>
2022-01-25 23:17:04 -0300

    Subindo projeto

---
+++ src/components/Message.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="message-container">
+    <p>{{ handleMsg(this.msg) }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Message",
+    data(){
+      return{
+        message: ""
+      }
+    },
+    props: {
+        msg: Number,
+      },
+      methods:{
+        handleMsg(msg){
+          console.log(this.msg)
+            switch(msg){
+              case 1 :
+                return this.message = "Pedido realizado com sucesso!"
+              break;
+              case 2 :
+               return this.message = ""
+              break;
+              case 3 :
+               return this.message = ""
+              break;
+            }
+            
+        },
+
+      }
+  }
+
+</script>
+
+<style scoped>
+  .message-container {
+    color: #004085;
+    background-color: #cce5ff;
+    border: 2px solid #b8daff;
+    border-radius: 5px;
+    padding: 10px;
+    max-width: 400px;
+    margin: 30px auto;
+  }
+</style>
